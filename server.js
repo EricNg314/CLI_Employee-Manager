13372dcb11883389c4feb9e22999eb7d70be390e
Okens <Ng.Eric314@gmail.com>
2022-04-16 16:39:54 -0700

    Adding initial setup.

---
+++ server.js
@@ -0,0 +1,33 @@
+// Enabling environment variables.
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const db = require('./db/connection');
+const apiRoutes = require('./routes/api');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Setting up express middleware
+app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+
+// Use api routes.
+app.use('/api', apiRoutes);
+
+// A default response for requests not found.
+app.use((req,res) => {
+  res.status(404).end()
+})
+
+
+// Start server once database is connected.
+db.connect(err => {
+  if(err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
+    console.log(`Server on http://localhost:${PORT}`);
+  })
+})
+
