c324b5c157c60f92a94bacdc5d3d438e2338d09c
Okens <Ng.Eric314@gmail.com>
2022-04-17 00:17:54 -0700

    Adding first query function for view all employees with async/await.

---
+++ routes/api/employees.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const { getAllEmployees } = require('../../lib/employees')
+
+router.get('/employees/viewAll', async function(req,res) {
+  console.log('entered route /employees/viewAll')
+  const info = await getAllEmployees();
+  // console.log('info')
+  console.log('info from getAllEmployees:', info)
+  console.log("info.message.includes('Error:'): ", info.message.includes('Error:'))
+  if(info.message.includes('Error:')){
+    res.status(500).json({error: info})
+  } else {
+    console.log("success")
+    res.json({
+      message:'success',
+      data: info
+    });
+  }
+})
+
+module.exports = router;
