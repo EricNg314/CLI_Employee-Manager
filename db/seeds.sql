1aebfa8b99ef92c5b8519843bd0e889e4dc23ddf
Okens <Ng.Eric314@gmail.com>
2022-04-20 18:59:52 -0700

    Adding more seeds for testing.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,24 +1,30 @@
 USE employee_db;
 INSERT INTO department (name) 
 VALUES
+  ('Council'),
   ('Finance'),
-  ('Sales');
+  ('Sales'),
+  ('Research');
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-  ('Finance Manager', 110000, 1),
-  ('Jr. Accountant', 45000, 1),
-  ('Sales Manager', 100000, 2),
-  ('Jr. Sales Associate', 40000, 2);
+  ('President', 1000000000, 1),
+  ('Finance Manager', 110000, 2),
+  ('Jr. Accountant', 45000, 2),
+  ('Sales Manager', 100000, 3),
+  ('Jr. Sales Associate', 40000, 3),
+  ('Inventor', 1000000, 4);
 
 -- Adding Managers
 INSERT INTO employee (first_name, last_name, role_id)
 VALUES 
-  ('Paul', 'Cézanne', 1),
-  ('Pierre-Auguste', 'Renoir', 3);
+  ('Paul', 'Cézanne', 2),
+  ('Pierre-Auguste', 'Renoir', 4),
+  ('Morty', '304-X', 1),
+  ('Rick', 'C-137', 5);
 
 -- Adding employees
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-  ('Tiger', 'Lake', 2, 1),
-  ('Snow', 'Ridge', 4, 2);
+  ('Tiger', 'Lake', 3, 1),
+  ('Snow', 'Ridge', 5, 2);
